00a93fbeab25e0226eb1b085d7b29a1aa3c00e35
Heloísa Tanquella <142849538+Heloisatanquella@users.noreply.github.com>
2023-10-17 09:43:50 -0300

    Update Exercício 5.py

--- Exercício 5.py
+++ Exercício 5.py
@@ -1,15 +1,12 @@
 #If, else, elif
 
-#idade = int(input('Informe sua idade:'))
+idade = int(input('Informe sua idade:'))
 
-#if idade >= 60:
-    #print('Melhor idade')
+if idade >= 60:
+    print('Melhor idade')
 
-#elif idade >= 18:
-    #print('Maior de idade')
+elif idade >= 18:
+    print('Maior de idade')
 
-#else:
-    #print('Menor de idade')
-
-nome = ('Helo')
-print = (nome.capitalize)
+else:
+    print('Menor de idade')
